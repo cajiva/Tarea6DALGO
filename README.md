6a497488550e75dc60f8f822412536fab096db78
Catalina Jimenez <c.jimenezv@uniandes.edu.co>
2021-04-24 12:12:39 -0500

    Timers para Dijkstra y BF

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Tarea6DALGO
+
+#Parte 1
+Tiempos de Ejecución:
+1. 
+2. Distancias100:
+	BellmanFord (milliseconds): 78
+	Dijkstra (milliseconds): 69
+	
--- src/parte1/MainParte1.java
+++ src/parte1/MainParte1.java
@@ -13,7 +13,7 @@ public class MainParte1 {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		File grafo = new File ("./data/distances5.txt");
+		File grafo = new File ("./data/distances100.txt");
 		Scanner scanner = new Scanner(grafo);
 
 		ArrayList<Integer>[] adj = null; // lista con tamaño = # vertices con arreglos que llevan
@@ -52,36 +52,43 @@ public class MainParte1 {
 
 		int[][] mBF = new int[adj.length][adj.length]; // matriz costos minimos
 
+		long stBF = System.currentTimeMillis();
 		for (int i = 0; i < adj.length; i++) {
 			BellmanFord bf = new BellmanFord(adj, pesos, i);
 			int[] costosMin = bf.retornarCostosMin();
 			mBF[i] = costosMin; // le agrego la fila a la matriz
 		}
+		long etBF = System.currentTimeMillis();
+		
+		System.out.println("Tiempo ejecucion BellmanFord (milliseconds): "+(etBF-stBF));
 
-		System.out.println("Matriz Costos Mínimos BellmanFord");
-		for (int i = 0; i < adj.length; i++) {
-			for (int j = 0; j < adj.length; j++) {
-				System.out.printf("%5d",mBF[i][j]);
-			}
-			System.out.println();
-		}	
+//		System.out.println("Matriz Costos Mínimos BellmanFord");
+//		for (int i = 0; i < adj.length; i++) {
+//			for (int j = 0; j < adj.length; j++) {
+//				System.out.printf("%5d",mBF[i][j]);
+//			}
+//			System.out.println();
+//		}	
 
 		int[][] mD = new int[adj.length][adj.length]; // matriz costos minimos
 
+		long stD = System.currentTimeMillis();
 		for (int i = 0; i < adj.length; i++) {
 			Dijkstra dij = new Dijkstra(adj, pesos, i);
 			int[] costosMin = dij.retornarCostosMin();
 			mD[i] = costosMin; // le agrego la fila a la matriz
 		}
+		long etD = System.currentTimeMillis();
+		System.out.println("Tiempo ejecucion Dijkstra (milliseconds): "+(etD-stD));
 
-		System.out.println();
-		System.out.println("Matriz Costos Mínimos Dijkstra");
-		for (int i = 0; i < adj.length; i++) {
-			for (int j = 0; j < adj.length; j++) {
-				System.out.printf("%5d",mD[i][j]);
-			}
-			System.out.println();
-		}	
+//		System.out.println();
+//		System.out.println("Matriz Costos Mínimos Dijkstra");
+//		for (int i = 0; i < adj.length; i++) {
+//			for (int j = 0; j < adj.length; j++) {
+//				System.out.printf("%5d",mD[i][j]);
+//			}
+//			System.out.println();
+//		}	
 
 	}
 }
